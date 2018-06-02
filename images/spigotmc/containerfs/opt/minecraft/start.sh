bd231c88755ee2e37221b434685e84753f10a255
Matt Craven <matt@craven.cloud>
2018-06-02 22:31:46 +0100

    Added the start.sh file to run the java instances

---
+++ images/spigotmc/containerfs/opt/minecraft/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+java -Xmx${SRV_MAXHEAP}M -Xms${SRV_MINHEAP}M -XX:+UseConcMarkSweepGC \
+-XX:+CMSIncrementalPacing -XX:ParallelGCThreads=${SRV_CPUCOUNT} -XX:+AggressiveOpts \
+-jar ${JAR_FILENAME} nogui
