9c71cb5ea16a1f8e1a4a469f1124199090a459e8
Matt Craven <m@ttcraven.com>
2019-08-12 23:26:28 +0100

    Removed Cuberite as no longer supported

--- images/spigotmc/containerfs/opt/minecraft/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-java -Xmx${SRV_MAXHEAP}M -Xms${SRV_MINHEAP}M -XX:+UseConcMarkSweepGC \
--XX:+CMSIncrementalPacing -XX:ParallelGCThreads=${SRV_CPUCOUNT} -XX:+AggressiveOpts \
--jar ${JAR_FILENAME} nogui
