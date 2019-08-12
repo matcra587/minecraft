9d1e68001f21e788968bed61a4d4f0cee2076bec
Matt Craven <m@ttcraven.com>
2019-08-12 23:24:28 +0100

    Converted to Python3

--- images/vanilla/containerfs/opt/minecraft/minecraftCMD.py
+++ images/vanilla/containerfs/opt/minecraft/minecraftCMD.py
@@ -1,9 +1,9 @@
-import urllib2
+import urllib.request, urllib.error, urllib.parse
 import json
 
 def get_json_from_url(url):
-    req = urllib2.Request(url)
-    opener = urllib2.build_opener()
+    req = urllib.request.Request(url)
+    opener = urllib.request.build_opener()
     f = opener.open(req)
     return json.loads(f.read())
 
@@ -20,4 +20,4 @@ def get_latest_release():
     return json['latest']['release']
 
 latest_version = get_latest_release()
-print  get_version_of_jar(latest_version)
+print(get_version_of_jar(latest_version))
